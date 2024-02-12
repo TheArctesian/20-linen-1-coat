e7048bdfbce37d2ac6688bb7c25bf5baac32ea47
theArctesian <stephen.d.okita@gmail.com>
2024-02-11 23:53:46 -0800

    abstract.py

---
+++ abstract.py
@@ -0,0 +1,13 @@
+import csv
+
+with open('updated_dataset.csv', 'r') as csvfile:
+        reader = csv.reader(csvfile)
+        data = list(reader)
+
+dif = 0 # this is the abstract labour of dif between theoretical and actual price (it might be neg that will be a problem)
+count = 0
+for row in data[1:]:
+    dif += float(row[2]) - float(row[3])
+    count += 1
+
+print(dif)
